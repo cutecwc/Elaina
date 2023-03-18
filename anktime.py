421672eba4a02996a4a2b286c9723f3722bad13b
cutecwc <cwcsocute@gmail.com>
2023-03-18 23:31:26 +0800

    'py cr'

--- anktime.py
+++ anktime.py
@@ -42,6 +42,7 @@ def func_ReviseTest(nameextra, filepath, flags):
         f2.writelines(a)
     f2.close()
 
+
 def func_MyReMatch(filepath, nameextra):
     for filepathe, dirs, fs in os.walk(filepath):
         for f in fs:
@@ -52,9 +53,75 @@ def func_MyReMatch(filepath, nameextra):
                 # func_FileOpen(os.path.join(filepathe, f))
                 func_ReviseTest(os.path.join(filepathe, f))
 
-if __name__ == '__main__':
+
+# display by line, you should refine the catch by yourself.
+def func_FileReadByLine(filepath):
+    f1 = open(filepath,'r')
+    freadline = f1.readlines()
+    f1.close()
+    flag = 0
+    for eachline in freadline:
+        flag += 1
+        if flag <= 5:
+            continue
+        elif flag > 7:
+            break
+        else:
+            print(eachline)
+
+
+
+def func_FileBackupBeforeRevised(filepath):
+    f1 = open(filepath,'r')
+    freadline = f1.readlines()
+    f1.close()
+    with open("./new.md.backup","w") as file:
+        for eachline in freadline:
+            file.write(eachline)
+
+# ---------------------------------- #
+
+# this is an operation to revise time.
+def ops_RunTimeRevise():
     filepaths = "./content/posts/new.md"
+    print("====================================")
+    func_FileReadByLine(filepaths)
+    func_FileBackupBeforeRevised(filepaths)
     # func_ReMatch(r"date:\ \d{4}-\d{1,2}-\d{1,2}", filepaths)
     func_ReviseTest(r"date:\ \d{4}-\d{1,2}-\d{1,2}", filepaths, "date: ")
     func_ReviseTest(r"lastmod:\ \d{4}-\d{1,2}-\d{1,2}", filepaths, "lastmod: ")
+    print("------------------------------------")
+    func_FileReadByLine(filepaths)
+    print("====================================")
     # func_TimeGet(filepaths)
+
+# ================================== #
+import argparse
+import sys
+
+def args_Defination():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--use", "-u", help=" -u, -t ", action = "store_true")
+    # parser.add_argument('--year', '-y', help='year 属性，非必要参数，但是有默认值', default=2017)
+    # parser.add_argument('--body', '-b', help='body 属性，必要参数', required=True)
+    parser.add_argument("--time", "-t", help=" Runtime get by auto revision. Version 0.1 ", action = "store_true")
+    args = parser.parse_args()
+    runtimeflags = False
+    if args.use:
+        print("===============================\n>> helpsget")
+        runtimeflags =True
+        return
+    if args.time:
+        print("===============================\n>> time revised... ")
+        runtimeflags =True
+        ops_RunTimeRevise()
+        return
+    if runtimeflags==False:
+        print("===============================\n>> nothing done. you'd like to give an agument likes '-u'(known how to use it)")
+
+import datetime
+
+if __name__ == '__main__':
+    args_Defination()
+    print(">>", datetime.datetime.now() , "all done. ")
+    
